f749f34bbefacb5ee6291d5ee5cf53397bc46f72
vanithapothireddy-collab <vanithapothireddy@gmail.com>
2026-01-12 18:55:47 +0530

    Create sudoku-solver.py

---
+++ Task-03/Task-04/sudoku-solver.py
@@ -0,0 +1,51 @@
+def print_board(board):
+    for row in board:
+        print(row)
+
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num:
+            return False
+
+    box_x = col // 3 * 3
+    box_y = row // 3 * 3
+
+    for i in range(box_y, box_y + 3):
+        for j in range(box_x, box_x + 3):
+            if board[i][j] == num:
+                return False
+
+    return True
+
+def solve(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+board = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,7,9]
+]
+
+print("Unsolved Sudoku:")
+print_board(board)
+
+solve(board)
+
+print("\nSolved Sudoku:")
+print_board(board)
